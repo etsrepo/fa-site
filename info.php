4a6ac375c07aa1fc28f9e65c9ee4139014e0dee1
Karthik Bhat <karthik.ax.ax@gmail.com>
2017-04-27 07:30:24 +0530

    Working info.php

--- info.php
+++ info.php
@@ -1,1 +1,28 @@
 <?php
+
+$current_version = '1.0.1';
+$download_count = @file_get_contents('count.txt');
+$changes = [
+    '1.0.1' => [
+        'Windows, Mac Installer',
+        'Update checker',
+        'Configuration validator',
+        'Connection test button',
+    ],
+    '1.0.0' => [
+        'Create new multiple connections',
+        'Service Account Json importer',
+        'Delete connection',
+        'Toggle sidebar',
+        'Listing and selecting saved connections',
+        'Explorer and Query mode',
+        'Tree, Json and Raw output views',
+        'Tree view objects in sidebar',
+        'Sidebar Filtering',
+        'Copy button to copy output to clipboard',
+        'Preferences to change themes, Json theme and Fonts',
+        'Button to navigate to firebase console',
+        'Update checker link',
+        'Initial realease',
+    ],
+];
